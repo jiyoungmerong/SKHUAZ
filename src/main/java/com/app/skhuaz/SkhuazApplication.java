120ad072c98b14679de0c4faddbbbeb4fdb39f10
jiyoungmerong <jiyoungmerong@gmail.com>
2023-07-31 21:43:32 +0900

    초기설정

---
+++ src/main/java/com/app/skhuaz/SkhuazApplication.java
@@ -0,0 +1,13 @@
+package com.app.skhuaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkhuazApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkhuazApplication.class, args);
+    }
+
+}
