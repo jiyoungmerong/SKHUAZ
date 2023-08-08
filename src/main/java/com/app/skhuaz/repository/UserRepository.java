f945fdc28dd40ad4457686b93e2a3526f9d39bb9
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-08 22:33:38 +0900

    feat: Add UserRepository

---
+++ src/main/java/com/app/skhuaz/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.skhuaz.repository;
+
+import com.app.skhuaz.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
