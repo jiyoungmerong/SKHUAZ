202554dc58c4de87dc79793588d5ef36d51469ea
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-10 23:32:58 +0900

    feat: 로그인 요청(loginRequest) 추가

---
+++ src/main/java/com/app/skhuaz/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.app.skhuaz.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+public class LoginRequest {
+    private String email;
+
+    private String password;
+}
