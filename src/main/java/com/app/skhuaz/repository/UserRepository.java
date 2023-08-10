2a6737ad3e9afcad176856798eb0874687b8c18f
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-10 21:52:55 +0900

    feat: findByNickname 추가(for 회원가입 시 닉네임 중복 체크)

--- src/main/java/com/app/skhuaz/repository/UserRepository.java
+++ src/main/java/com/app/skhuaz/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.app.skhuaz.repository;
 
 import com.app.skhuaz.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
