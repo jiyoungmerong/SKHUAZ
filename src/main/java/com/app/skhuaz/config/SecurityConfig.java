0c946dccb79c316c000d2c9de350d7369efb4b36
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-10 23:33:47 +0900

    feat: PasswordEncoder 추가, filterChain에 filter 추가

--- src/main/java/com/app/skhuaz/config/SecurityConfig.java
+++ src/main/java/com/app/skhuaz/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.app.skhuaz.config;
 
+import com.app.skhuaz.jwt.service.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @RequiredArgsConstructor
@@ -14,14 +18,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeRequests()
-                .requestMatchers("/user/join").permitAll() // 회원가입은 인증하지 않은 모든 사용자 허용
-                .and()
-                .authorizeRequests()
-                .anyRequest().authenticated(); // 그 외의 요청은 인증된 사용자만 접근 가능
+                .authorizeRequests(requests -> requests
+                        .requestMatchers("/user/join").permitAll() // 회원가입은 인증하지 않은 모든 사용자 허용
+                        .requestMatchers("/user/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
