695275f1f4bbca2e8b65f0541ebd9c0bd22825af
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-10 23:34:33 +0900

    feat: 로그인 api 생성

--- src/main/java/com/app/skhuaz/controller/UserController.java
+++ src/main/java/com/app/skhuaz/controller/UserController.java
@@ -1,18 +1,28 @@
 package com.app.skhuaz.controller;
 
+import ch.qos.logback.core.status.ErrorStatus;
+import com.app.skhuaz.common.RspsTemplate;
+import com.app.skhuaz.domain.User;
 import com.app.skhuaz.exception.ErrorCode;
 import com.app.skhuaz.exception.exceptions.BusinessException;
+import com.app.skhuaz.jwt.dto.TokenDto;
+import com.app.skhuaz.jwt.service.TokenManager;
+import com.app.skhuaz.repository.UserRepository;
 import com.app.skhuaz.request.JoinRequest;
+import com.app.skhuaz.request.LoginRequest;
 import com.app.skhuaz.response.JoinResponse;
 import com.app.skhuaz.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,8 +31,12 @@ public class UserController {
 
     private final UserService userService;
 
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/join") // 회원가입
-    public ResponseEntity<JoinResponse> join(@RequestBody @Valid JoinRequest request){
+    public ResponseEntity<JoinResponse> join(@RequestBody @Valid final JoinRequest request){
         try {
             if (userService.checkDuplicateEmail(request.getEmail())) { // 이메일 중복시
                 throw new BusinessException(ErrorCode.EMAIL_ALREADY_REGISTERED);
@@ -32,12 +46,26 @@ public class UserController {
 
             JoinResponse response = userService.create(request);
 
-            // body에 어떤게 들어가는지 확실히 파악할 수 있게
             return ResponseEntity.status(HttpStatus.OK).body(response);
 
         } catch (Exception e) { // 그 밖의 예외 발생시
             // fixme 발생할 수 있는 다른 예외 처리
             throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping("/login") // 로그인
+    public ResponseEntity<TokenDto> login(@RequestBody @Valid final LoginRequest request) {
+        Optional<User> user = userRepository.findByEmail(request.getEmail());
+        if (user.isEmpty()) { // 유저가 존재하지 않을 때
+            throw new BusinessException(ErrorCode.USER_NOT_JOIN);
+        }
+        if (!passwordEncoder.matches(request.getPassword(), user.get().getPassword())) { // 비밀번호가 옳지 않을 때
+            throw new BusinessException(ErrorCode.USER_CERTIFICATION_FAILED);
+        }
+
+        TokenDto tokenDto = userService.login(request.getEmail(), request.getPassword());
+
+        return ResponseEntity.ok(tokenDto);
     }
 }
