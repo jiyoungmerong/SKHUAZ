482bec44de0edd5abd5b169c66f1d375471b0491
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-15 20:55:41 +0900

    feat: 응답 성공 템플릿 추가

---
+++ src/main/java/com/app/skhuaz/common/RspsTemplate.java
@@ -0,0 +1,26 @@
+package com.app.skhuaz.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class RspsTemplate<T> {
+    private int statusCode;
+    private T data;
+
+    private String message;
+
+    public RspsTemplate(HttpStatus httpStatus, T data) {
+        this.statusCode = httpStatus.value();
+        this.data = data;
+    }
+
+    public RspsTemplate(T data) {
+        this.data = data;
+    }
+
+    public RspsTemplate(HttpStatus httpStatus, String message){
+        this.statusCode = httpStatus.value();
+        this.message = message;
+    }
+}
