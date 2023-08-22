6be41c4bfd1edec4394d134c2a6e9f889d904118
jiyoungmerong <jiyoungmerong@gmail.com>
2023-08-22 12:40:25 +0900

    chore: return passwordEncorder 변경

--- src/main/java/com/app/skhuaz/config/SecurityConfig.java
+++ src/main/java/com/app/skhuaz/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,14 +22,14 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeRequests(requests -> requests
-                        .requestMatchers("/user/join").permitAll() // 회원가입은 인증하지 않은 모든 사용자 허용
+                        .requestMatchers("/user/join").permitAll() // 인증하지 않은 모든 사용자 허용
                         .requestMatchers("/user/login").permitAll()
                         .requestMatchers("/user/checkDuplicate/{nickname}").permitAll()
                         .requestMatchers("/email/send").permitAll()
