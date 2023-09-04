a3c03e6966ed55ee26bb23d012f7f57b3028f699
jiyoungmerong <jiyoungmerong@gmail.com>
2023-09-04 12:35:16 +0900

    feat: 강의평 repository

---
+++ src/main/java/com/app/skhuaz/repository/EvaluationRepository.java
@@ -0,0 +1,7 @@
+package com.app.skhuaz.repository;
+
+import com.app.skhuaz.domain.Evaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+}
