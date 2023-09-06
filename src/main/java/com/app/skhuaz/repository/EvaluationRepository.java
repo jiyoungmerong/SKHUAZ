973c89419cb6429efd5b210bc1c7e7129947da69
jiyoungmerong <jiyoungmerong@gmail.com>
2023-09-06 21:25:14 +0900

    feat: 레포지토리 추가

--- src/main/java/com/app/skhuaz/repository/EvaluationRepository.java
+++ src/main/java/com/app/skhuaz/repository/EvaluationRepository.java
@@ -1,7 +1,16 @@
 package com.app.skhuaz.repository;
 
 import com.app.skhuaz.domain.Evaluation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+    Page<Evaluation> findAll(Pageable pageable);
+
+    List<Evaluation> findAllByEmail(String email);
+
+    Evaluation findByEvaluationId(Long evaluationId);
 }
