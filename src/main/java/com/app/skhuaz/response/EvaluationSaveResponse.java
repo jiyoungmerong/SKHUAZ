f90ffe380d33b070b65d9b97b9c9f3a1ea1840a5
jiyoungmerong <jiyoungmerong@gmail.com>
2023-09-06 21:26:07 +0900

    chore: 강의평 저장 resoponse 수정

--- src/main/java/com/app/skhuaz/response/EvaluationSaveResponse.java
+++ src/main/java/com/app/skhuaz/response/EvaluationSaveResponse.java
@@ -15,14 +15,14 @@ public class EvaluationSaveResponse {
     private int task; // 과제정도
     private int practice; // 실습정도
     private int presentation; // 발표 정도
+    private String title;
     private String review; // 총평
-    private String nickname;
 
     public static EvaluationSaveResponse of(String deptName, String lecName, String profName,
                                             int semester, int teamPlay, int task, int practice,
-                                            int presentation, String review, String nickname){
+                                            int presentation, String title, String review){
         return new EvaluationSaveResponse(deptName, lecName, profName,
-                semester, teamPlay, task, practice, presentation, review, nickname);
+                semester, teamPlay, task, practice, presentation, title, review);
     }
 
 }
