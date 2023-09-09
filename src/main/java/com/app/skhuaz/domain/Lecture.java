c5c78dbfd1e96265751fee278a523e9a87d40c62
jiyoungmerong <jiyoungmerong@gmail.com>
2023-09-09 13:26:28 +0900

    feat: 강의 저장할 Lectrue 엔티티 생성

---
+++ src/main/java/com/app/skhuaz/domain/Lecture.java
@@ -0,0 +1,39 @@
+package com.app.skhuaz.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import static lombok.AccessLevel.PROTECTED;
+
+/*
+강의평 작성 강의
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Table(name="Lecture")
+@AllArgsConstructor(access = PROTECTED)
+public class Lecture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long lectureId;
+
+    private String deptName; // 학과 이름 (ex, 소프 컴공 ... )
+
+    private String lecName; // 강의 이름
+
+    private String profName; // 교수님
+
+    private int semester; // 개설 학기
+
+    @Builder Lecture(String deptName, String lecName, String profName, int semester){
+        this.deptName = deptName;
+        this.lecName = lecName;
+        this.profName = profName;
+        this.semester = semester;
+    }
+}
