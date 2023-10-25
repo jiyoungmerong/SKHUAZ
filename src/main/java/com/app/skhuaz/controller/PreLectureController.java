9028f3007a85b2755e5ac31062a59dc6481b557d
jiyoungmerong <jiyoungmerong@gmail.com>
2023-10-26 00:05:18 +0900

    chore: 선수과목 저장 로직 수정

--- src/main/java/com/app/skhuaz/controller/PreLectureController.java
+++ src/main/java/com/app/skhuaz/controller/PreLectureController.java
@@ -2,9 +2,7 @@ package com.app.skhuaz.controller;
 
 import com.app.skhuaz.common.RspsTemplate;
 import com.app.skhuaz.domain.PreLecture;
-import com.app.skhuaz.response.PrerequisitesResponse;
 import com.app.skhuaz.service.PreLectureService;
-import com.app.skhuaz.service.SoftwareSubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +14,11 @@ import java.util.List;
 public class PreLectureController {
     private final PreLectureService preLectureService;
 
-    private final SoftwareSubjectService softwareSubjectService;
-
-    @PostMapping("/preLecture/create") // 선수과목 저장
-    public RspsTemplate<List<PreLecture>> addLectures(@RequestBody List<PreLecture> lectures, Principal principal) {
-        return preLectureService.addLectures(lectures, principal.getName());
+    @PostMapping("/preLecture/create")
+    public RspsTemplate<List<PreLecture>> addPreLectures(Principal principal) {
+        return preLectureService.addLectures(principal.getName());
     }
+
 
     @GetMapping("/preLecture/check") // 로그인한 유저 선수과목 불러오기
     public RspsTemplate<List<PreLecture>> getPreLectures(Principal principal) {
--- src/main/java/com/app/skhuaz/service/PreLectureService.java
+++ src/main/java/com/app/skhuaz/service/PreLectureService.java
@@ -2,12 +2,16 @@ package com.app.skhuaz.service;
 
 import com.app.skhuaz.common.RspsTemplate;
 import com.app.skhuaz.domain.PreLecture;
+import com.app.skhuaz.domain.SoftwareSubject;
 import com.app.skhuaz.repository.PreLectureRepository;
+import com.app.skhuaz.repository.SoftwareSubjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -16,16 +20,30 @@ import java.util.List;
 public class PreLectureService {
     private final PreLectureRepository preLectureRepository;
 
+    private final SoftwareSubjectRepository softwareSubjectRepository;
+
     @Transactional
-    public RspsTemplate<List<PreLecture>> addLectures(List<PreLecture> lectures, String email) {
-        for (PreLecture lecture : lectures) {
-            lecture.addUser(email);
+    public RspsTemplate<List<PreLecture>> addLectures(String email) {
+
+        // checkYn이 true인 컬럼을 가져옴
+        List<SoftwareSubject> softwareSubjects = softwareSubjectRepository.findByCheckYnTrue();
+
+        List<PreLecture> preLectures = new ArrayList<>();
+
+        for (SoftwareSubject softwareSubject : softwareSubjects) {
+            PreLecture preLecture = PreLecture.builder()
+                    .semester(softwareSubject.getSemester())
+                    .email(email)
+                    .lecNames(Collections.singletonList(softwareSubject.getSubjectName()))
+                    .build();
+
+            preLectures.add(preLecture);
         }
 
-        preLectureRepository.saveAll(lectures);
-        return new RspsTemplate<>(HttpStatus.OK, "선수과목 저장에 성공하였습니다.", lectures);
-
+        preLectureRepository.saveAll(preLectures);
+        return new RspsTemplate<>(HttpStatus.OK, "선수과목 저장에 성공하였습니다.", preLectures);
     }
+
 
     public RspsTemplate<List<PreLecture>> getPreLecturesByUser(String userEmail) {
         // 사용자의 이메일을 기반으로 해당 사용자가 저장한 선수 과목 정보를 조회
