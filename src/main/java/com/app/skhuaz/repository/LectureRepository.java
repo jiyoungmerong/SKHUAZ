0a557b33441e465d4b6b44445f403d14611f2b84
jiyoungmerong <jiyoungmerong@gmail.com>
2023-10-25 22:41:55 +0900

    feat: 강의이름, 교수님이름, 학기 조회 쿼리 추가

--- src/main/java/com/app/skhuaz/repository/LectureRepository.java
+++ src/main/java/com/app/skhuaz/repository/LectureRepository.java
@@ -10,19 +10,15 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    Optional<Lecture> findByDeptNameAndLecNameAndProfNameAndSemester(
-            String deptName, String lecName, String profName, String semester);
+    @Query("SELECT DISTINCT l FROM Lecture l WHERE l.lecName = :lecName AND l.profName = :profName AND l.semester = :semester")
+    Optional<Lecture> findByLecNameAndProfNameAndSemester(
+            @Param("lecName") String lecName,
+            @Param("profName") String profName,
+            @Param("semester") String semester
+    );
 
     @Query("SELECT DISTINCT l.semester FROM Lecture l")
     List<String> findAllSemesters();
-
-//    @Query("SELECT DISTINCT l.profName FROM Lecture l WHERE l.semester = :semester")
-//    List<String> findProfessorsBySemester(@Param("semester") String semester);
-//
-//    @Query("SELECT l.lecName FROM Lecture l WHERE l.semester = :semester AND l.profName = :professorName")
-//    List<String> findLectureNameBySemesterAndProfessor(
-//            @Param("semester") String semester,
-//            @Param("professorName") String professorName);
 
     @Query("SELECT DISTINCT l.lecName FROM Lecture l WHERE l.semester = :semester")
     List<String> findLecturesBySemester(@Param("semester") String semester);
