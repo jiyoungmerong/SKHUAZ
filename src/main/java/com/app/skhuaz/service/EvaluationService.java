572dc1a4a9f32aa2adeb289e61f8533c5aa6cad2
jiyoungmerong <jiyoungmerong@gmail.com>
2023-10-27 12:22:10 +0900

    chore: 사용하지 않는 라이브러리 삭제

--- src/main/java/com/app/skhuaz/service/EvaluationService.java
+++ src/main/java/com/app/skhuaz/service/EvaluationService.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
