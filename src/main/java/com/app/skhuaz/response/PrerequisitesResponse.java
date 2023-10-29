fb8997c541df594e5232eb23ea8180c0c41e7f2d
jiyoungmerong <jiyoungmerong@gmail.com>
2023-10-29 23:33:43 +0900

    chore: 불필요한 메소드 삭제

--- src/main/java/com/app/skhuaz/response/PrerequisitesResponse.java
+++ src/main/java/com/app/skhuaz/response/PrerequisitesResponse.java
@@ -12,15 +12,13 @@ import java.util.Comparator;
 @Getter
 @Setter
 public class PrerequisitesResponse {
+
+    private final Long preId;
     private final String subjectName;
     private final String subjectSemester;
 
-    public static PrerequisitesResponse of(String subjectName, String subjectSemester) {
-        return new PrerequisitesResponse(subjectName, subjectSemester);
-    }
-
-    public static PrerequisitesResponse from(String preName, String preSemester) {
-        return new PrerequisitesResponse(preName, preSemester);
+    public static PrerequisitesResponse of(Long preId, String subjectName, String subjectSemester) {
+        return new PrerequisitesResponse(preId, subjectName, subjectSemester);
     }
 
     public static Comparator<PrerequisitesResponse> semesterComparator = Comparator.comparing(o -> parseSemester(o.subjectSemester));
@@ -30,20 +28,3 @@ public class PrerequisitesResponse {
         return Integer.parseInt(semester.replace("학년 ", "").replace("학기", ""));
     }
 }
-
-//public class PrerequisitesResponse {
-//    private String subjectName;
-//    private String subjectSemester;
-//
-//    private String preName;
-//    private String preSemester;
-//
-//    public static PrerequisitesResponse of(String subjectName, String subjectSemester) {
-//        return new PrerequisitesResponse(subjectName, subjectSemester);
-//    }
-//    public static PrerequisitesResponse from(String preName, String preSemester) {
-//        return new PrerequisitesResponse(preName, preSemester);
-//    }
-//
-//
-//}
